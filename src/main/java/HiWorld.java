8b7f234274964527fa951a9e1769ce1e36f87e89
Ashley Caselli <ashley.caselli@studio.unibo.it>
2017-07-11 11:54:10 +0200

    Modify

--- src/main/java/HiWorld.java
+++ src/main/java/HiWorld.java
@@ -2,7 +2,7 @@ public class HiWorld
 {
     public static void main(String [] args)
     {
-        System.out.print("Buongiorno Mondo");
+        System.out.print("Buongiorno Mondooooooo");
     }
 
 }
